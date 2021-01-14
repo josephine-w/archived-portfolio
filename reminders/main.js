f38d6097ef88d9ce6f72cabf002340b1f28d15fc
Josephine Widjaja <75404368+josephine-w@users.noreply.github.com>
2021-01-14 02:41:26 -0500

    update site

--- reminders/main.js
+++ reminders/main.js
@@ -1,45 +1,36 @@
-var myList = document.getElementsByTagName("LI");
+// create x button for each element
+var myList = document.getElementsByTagName("li");
 var x;
 for (x = 0; x < myList.length; x++) {
-    var span = document.createElement("span");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myList[i].appendChild(span);
+  var item = document.createTextNode("\u2715");
+  var span = document.createElement("span");
+  span.className = "close";
+  span.appendChild(item);
 }
 
+// close function
 var close = document.getElementsByClassName("close");
 var x;
-for (i = 0; i< close.length; i++) {
-    close[i].onclick = function() {
+for (x = 0; x < close.length; x++) {
+    close[x].onclick = function() {
         var div = this.parentElement;
         div.style.display = "none";
     }
 }
 
-var list = document.querySelector('ul');
-list.addEventListener('click',function(ev) {
-    if (ev.target.tagName == "LI") {
-        ev.target.classList.toggle('checked');
-    }
-}, false);
-
+// add to list
 function newElement() {
     var li = document.createElement("li");
     var inputValue = document.getElementById("userinput").value;
     var t = document.createTextNode(inputValue);
     li.appendChild(t);
-    if (inputValue === '') {
-      alert("Please input a value.");
-    } else {
-      document.getElementById("reminders").appendChild(li);
-    }
+    document.getElementById("reminders").appendChild(li);
     document.getElementById("userinput").value = "";
   
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
+    var item = document.createTextNode("\u2715");
+    var span = document.createElement("span");
     span.className = "close";
-    span.appendChild(txt);
+    span.appendChild(item);
     li.appendChild(span);
   
     for (x = 0; x < close.length; x++) {
